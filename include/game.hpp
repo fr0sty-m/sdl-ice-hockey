5712c61c45dbca3b1baaba8ee163985e2e7349e4
fr0sty-m <mertcan.053@hotmail.com>
2023-01-23 06:00:28 +0300

    Update game.hpp

--- include/game.hpp
+++ include/game.hpp
@@ -31,28 +31,61 @@ namespace fr0sty {
         public:
             Game() = default;
 
+            /// @brief A method for initializing everything
             void init();
+
+            /// @brief A method for initializing variables
+            /// @return True if everything initialized correctly
             bool initVars();
+
+            /// @brief A method for initializing window
+            /// @return True if everything initialized correctly
             bool initWindow();
 
+            /// @brief A method for cleaning and exiting program
             void quit();
             
+            /// @brief A method for updating
+            /// @param dt Delta Time
             void update(float dt);
+
+            /// @brief A method for pre rendering
             void preRender();
+
+            /// @brief A method for rendering
             void render();
+
+            /// @brief A method for post rendering
             void postRender();
+
+            /// @brief A method for polling events
             void events();
 
+            /// @brief A getter method for getting running variable
+            /// @return boolean running
             bool getRunning() { return running; }
 
+            /// @brief A setter method for delta time
+            /// @param dt Parameter for setting delta time
             void setDT(float dt) { _dt = dt; }
+            /// @brief A getter method for delta time
+            /// @return float dt
             float getDT() { return _dt;}
 
             Clock* getClock() { return &_clock; }
 
-            void write(std::string msg, bool endl) { if (endl) std::cout << msg << std::endl; else std::cout << msg; }
-            void write(float msg, bool endl) { if (endl) std::cout << msg << std::endl; else std::cout << msg; }
-            void write(int msg, bool endl) { if (endl) std::cout << msg << std::endl; else std::cout << msg; }
+            /// @brief A method for console output 
+            /// @param msg Parameter for output message
+            /// @param endl Parameter for new line or not
+            void write(std::string msg, bool endl) { if (endl) std::cout << "SYSTEM: " << msg << std::endl; else std::cout << "SYSTEM: " << msg; }
+            /// @brief A method for console output 
+            /// @param msg Parameter for output message
+            /// @param endl Parameter for new line or not
+            void write(float msg, bool endl) { if (endl) std::cout << "SYSTEM: " << msg << std::endl; else std::cout << "SYSTEM: " << msg; }
+            /// @brief A method for console output 
+            /// @param msg Parameter for output message
+            /// @param endl Parameter for new line or not
+            void write(int msg, bool endl) { if (endl) std::cout << "SYSTEM: " << msg << std::endl; else std::cout << "SYSTEM: " << msg; }
 
         };	// 		 !Game
 }	// 		 !fr0sty
